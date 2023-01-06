767e546b2c6ae36a2786fda05ed4364c30364262
bot <hcm1476@gmail.com>
2023-01-06 01:36:27 -0800

    added gui!

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## art
 
-NFT artwork generation.
+NFT artwork generation. 
 
 This code is generating NFT art by combining different images and then saving them in the 'Output' directory. The images are combined using the Pillow library's paste function and resized using the resize function. The resulting image is saved in the 'Output' directory with the file name being a concatenation of the attribute values.
 
@@ -8,54 +8,4 @@ The code creates a class called NFT which takes in six attributes, attr0 through
 
 The generate function first maps the values of the attributes to corresponding descriptions using lists. It then opens the images corresponding to the attribute values using the Pillow library's open function and combines them using the paste function. The resulting image is then resized using the resize function and saved to the 'Output' directory.
 
-## generate(1,2,3,4,3,1)
-
-Generates a specific image.
-
-![123431_white_stoned_frown_small_sunset_adam](https://user-images.githubusercontent.com/32826270/204912590-5eef9865-fb71-4470-9175-fbe0217f3ba8.png)
-
-## generate_random(4)
-
-Generates 4 random images and saves them in /Output.
-
-![145145_white_tired_smile_crooked_snow_hair](https://user-images.githubusercontent.com/32826270/204912407-94c5d820-617e-4ccc-96e8-f97bc090bcdf.png)
-![233415_green_sus_frown_small_beach_hair](https://user-images.githubusercontent.com/32826270/204912409-16fad186-a2ff-498c-a62d-9653d3bd2f78.png)
-![242154_green_tired_smirk_crooked_morning_chest](https://user-images.githubusercontent.com/32826270/204912411-6a3d769b-cfb2-44e7-9c44-595815e85e18.png)
-![255222_green_sunglasses_smile_round_hell_ear](https://user-images.githubusercontent.com/32826270/204912413-d33a54f9-8ad4-4838-8292-dd6a4fcbdd78.png)
-
-## generate_all()
-
-Generates all permutations of images and saves them in /Output.
-
 ![output](https://user-images.githubusercontent.com/32826270/210912088-f77e66f3-2955-47f3-a572-1eac57c8d8a3.png)
-
-
-## clear
-
-Deletes all files in /Output.
-
-## output()
-
-Prints the names of .png files in /Output.
-```
-/usr/local/bin/python3.10 /Users/hcm444/PycharmProjects/art/main.py 
-242154_green_tired_smirk_crooked_morning_chest.png
-233415_green_sus_frown_small_beach_hair.png
-145145_white_tired_smile_crooked_snow_hair.png
-255222_green_sunglasses_smile_round_hell_ear.png
-
-Process finished with exit code 0
-
-```
-
-## find(attribute)
-
-Search by attribute.
-
-## delete(attribute)
-
-Delete by attribute.
-
-## show()
-
-Display images in /Output.
--- generate.py
+++ generate.py
@@ -1,15 +1,11 @@
 from itertools import product
-import random
-from PIL import Image
-import os
-import shutil
 
-from matplotlib import pyplot as plt
+from PIL import Image
 
 PATH: str = 'Output'
 ITERATION_MAX = 15624  # max permutations
-IMAGE_X = 25  # image size
-IMAGE_Y = 25
+IMAGE_X = 75  # image size
+IMAGE_Y = 75
 GEN_ARR = (list(product([1, 2, 3, 4, 5],
                         [1, 2, 3, 4, 5],
                         [1, 2, 3, 4, 5],
@@ -70,81 +66,4 @@ def generate(attr0, attr1, attr2, attr3, attr4, attr5):
                                                                          body_value,
                                                                          eyes_value, mouth_value, nose_value,
                                                                          background_value, extra_value), "PNG")
-
-
-def generate_all():
-    # five arrays for every attribute
-    # M is a list of every possible tuple
-    for counter in range(GEN_ARR.__len__()):
-        # parse through these tuples
-        # save every respective place of the tuple for generate()
-        NFT(GEN_ARR[counter][0], GEN_ARR[counter][1], GEN_ARR[counter][2],
-            GEN_ARR[counter][3], GEN_ARR[counter][4], GEN_ARR[counter][5])
-        # remember how many permutations you have! ex: 5x5x5x5x5 = 3125 files!
-
-
-def generate_random(generation_val):
-    # five arrays for every attribute
-    # M is a list of every possible tuple
-    for counter in range(generation_val):
-        # generate a number of random NFTs
-        counter = random.randint(0, ITERATION_MAX)
-        # random integer from beginning to end of possible iterations
-        # parse through these tuples
-        # save every respective place of the tuple for generate()
-        NFT(GEN_ARR[counter][0], GEN_ARR[counter][1], GEN_ARR[counter][2],
-            GEN_ARR[counter][3], GEN_ARR[counter][4], GEN_ARR[counter][5])
-        # remember how many permutations you have! ex: 5x5x5x5x5 = 3125 files!
-
-
-def output():
-    for images in os.listdir(PATH):
-        if images.endswith(".png"):
-            print(images)
-
-
-def find(attribute):
-    # search by attribute
-    result = []
-    for files in os.listdir(PATH):
-        if attribute in files:
-            result.append(os.path.join(PATH, files))
-    print(result)
-
-
-def delete(attribute):
-    # deletes by specific attribute "white", "21333", "fat", etc
-    for files in os.listdir(PATH):
-        if attribute in files:
-            file_path = os.path.join(PATH, files)
-            os.remove(file_path)
-            # it works don't change this method
-
-
-def show():
-    for files in os.listdir(PATH):
-        im = Image.open('{0}/{1}'.format(PATH, files))
-        plt.imshow(im)
-        plt.show()
-
-
-def clear():
-    # deletes all files in NFT/Output
-    for filename in os.listdir(PATH):
-        file_path = os.path.join(PATH, filename)
-        try:
-            if os.path.isfile(file_path) or os.path.islink(file_path):
-                os.unlink(file_path)
-            elif os.path.isdir(file_path):
-                shutil.rmtree(file_path)
-        except Exception as e:
-            print('Failed to delete %s. Reason: %s' % (file_path, e))
-
-
-clear()
-# clear files
-generate_random(500)
-# generate ten random images
-output()
-# print the generated files
 
